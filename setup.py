f39c7db31584db5977d0f5661ee2166c85c3115e
parsa alian <parsaalian0@gmail.com>
2018-07-17 12:50:47 +0430

    basic structure with setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='windi',
+      version='0.1',
+      description='Wrapper for pyindi',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+      ],
+      keywords='pyindi pyindi-client camera',
+      url='https://gitlab.com/parsaalian0/windi',
+      author='Parsa Alian',
+      author_email='parsaalian0@gmail.com',
+      license='MIT',
+      packages=['windi'],
+      zip_safe=False)
