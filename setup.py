ad6d5dcbcf9c7edc8785a5d93f9ce9b8919955ec
Unknown <parsaalian0@gmail.com>
2018-07-23 06:09:05 -0400

    agent completed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='windi',
-      version='1.1.0',
+      version='1.2.0',
       description='Wrapper for pyindi',
       classifiers=[
             'Development Status :: 3 - Alpha',
